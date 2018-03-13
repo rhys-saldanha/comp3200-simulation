ea6b3a6301d4557460200c278da6e334d35cc8c3
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-03-13 16:26:16 +0000

    [main] Update to use new mutation adding
    * new type with longer simulation

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from simulation import Simulation
 from type import Type
 
-if __name__ == "__main__":
-    a = Type("A", 400.0, 1.0, 0.1)
-    b = Type("B", 0, 1.0, 0.1)
-    a.add_mutations(0.1, (b, 0.9))
-    sim = Simulation(a, b)
-    sim.run(20.0)
+if __name__ == '__main__':
+    # === Types === #
+    a = Type('A', 10, 1.0, 0.7)
+    b = Type('B', 0, 1.0, 0.5)
+    c = Type('C', 0, 1.0, 0.3)
+
+    # === Mutations === #
+    a.add_mutation(a, 0.999)
+    a.add_mutation(b, 0.001)
+
+    b.add_mutation(b, 0.999)
+    b.add_mutation(c, 0.001)
+
+    c.add_mutation(c, 1.0)
+
+    # === Simulation === #
+    sim = Simulation(a, b, c, max=200)
+
+    sim.run(100.0)
