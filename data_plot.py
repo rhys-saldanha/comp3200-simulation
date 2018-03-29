acd0d275046c95bb39c5d5fa8a66b65e81a2de13
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-03-29 17:45:30 +0100

    [PLT] Plot the size history of a list of types
    * up to some time
    * decoupled from simulation

--- data_plot.py
+++ data_plot.py
@@ -1,53 +1,47 @@
+from typing import List
+
 import matplotlib.pyplot as plt
-import numpy as np
 from deprecated import deprecated
 
-from simulation import Simulation
+from type import Type
 
 
 class Plotter:
     @deprecated
-    def plot(self, simulation: Simulation, data=True):
+    def plot(self, types: List[Type], tmax: int):
         """
         Types do not have a statistics history anymore.
         This kind of statistics graph was not useful for analysis.
 
-        :param Simulation simulation: Simulation object for data
-        :param bool data: If `True` will graph the population size of all types
+        :param int tmax: maximum time of simulation
+        :param List[Type] types: list of types to plot
         """
-        stats = False
+        plt.title("Plot of type size over time")
+        plt.xlabel("time")
+        plt.ylabel("Number of type")
+        legend_list = []
+        for e in types:
+            v, t = list(zip(*e.history))
+            plt.scatter(t, v, marker='x')
+            legend_list.append("{}: size".format(e.full_name))
+        plt.legend(legend_list, loc='upper left')
+        plt.xlim(xmin=0.0, xmax=tmax)
+        plt.ylim(ymin=0)
 
-        if data:
-            plt.title("Plot of type size over time")
-            plt.xlabel("time")
-            plt.ylabel("Number of type")
-            legend_list = []
-            for e in simulation.types:
-                v, t = list(zip(*e.history))
-                plt.scatter(t, v, marker='x')
-                legend_list.append("{}: size".format(e.full_name))
-            plt.legend(legend_list, loc='upper left')
-            plt.xlim(xmin=0.0, xmax=simulation.tmax)
-            plt.ylim(ymin=0)
-
-        if data and stats:
-            plt.figure()
-
-        if stats:
-            plt.title("Plot of type statistics over time")
-            plt.xlabel("time")
-            plt.ylabel("Number of type")
-            legend_list = []
-            cmap = plt.get_cmap('tab10')
-            for i, e in enumerate(simulation.types):
-                m, v, t = list(zip(*e.stats_history))
-                v = list(map(lambda x: np.sqrt(x), v))
-                plt.plot(t, m, linestyle="--", c=cmap(i))
-                plt.plot(t, v, linestyle=":", c=cmap(i))
-                legend_list.append("{}: mean".format(e.full_name))
-                legend_list.append("{}: standard deviation".format(e.full_name))
-            plt.legend(legend_list, loc='upper left')
-            plt.xlim(xmin=0.0, xmax=simulation.tmax)
-            plt.ylim(ymin=0)
+        # plt.title("Plot of type statistics over time")
+        # plt.xlabel("time")
+        # plt.ylabel("Number of type")
+        # legend_list = []
+        # cmap = plt.get_cmap('tab10')
+        # for i, e in enumerate(simulation.__types):
+        #     m, v, t = list(zip(*e.stats_history))
+        #     v = list(map(lambda x: np.sqrt(x), v))
+        #     plt.plot(t, m, linestyle="--", c=cmap(i))
+        #     plt.plot(t, v, linestyle=":", c=cmap(i))
+        #     legend_list.append("{}: mean".format(e.full_name))
+        #     legend_list.append("{}: standard deviation".format(e.full_name))
+        # plt.legend(legend_list, loc='upper left')
+        # plt.xlim(xmin=0.0, xmax=simulation.__tmax)
+        # plt.ylim(ymin=0)
 
         plt.show()
