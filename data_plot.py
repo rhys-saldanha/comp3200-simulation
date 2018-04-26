45733debf9894b121ba91432196fd2c2783c4336
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-04-26 20:42:06 +0100

    [PLT] Some changes
    * use paths no Simulation objects
    * reduce resolution of graphs

--- data_plot.py
+++ data_plot.py
@@ -25,7 +25,8 @@ def line_plot(types: List[Type], tmax: float, size_max: int, plt=plt):
     plt.ylabel("Number of type")
 
     num_points = len(types[0].history)
-    reduce = int(num_points / (resolution * tmax))
+    # reduce = int(num_points / (resolution * tmax))
+    reduce = int(num_points / (resolution))
 
     legend_list = []
     for e in types:
@@ -42,7 +43,8 @@ def stacked_plot(types: List[Type], tmax: float, size_max: int, plt=plt):
     plt.ylabel("Number of type")
 
     num_points = len(types[0].history)
-    reduce = int(num_points / (resolution * tmax))
+    # reduce = int(num_points / (resolution * tmax))
+    reduce = int(num_points / (resolution))
 
     times = types[0].get_times[::reduce]
     histories = [t.get_sizes[::reduce] for t in types]
@@ -56,27 +58,30 @@ def stacked_plot(types: List[Type], tmax: float, size_max: int, plt=plt):
 
 def network_with_dominant(sim: Simulation, nx=nx) -> None:
     G = nx.Graph()
-
-    G.add_nodes_from(sim.get_types)
-
-    G.add_edges_from(list(itertools.chain(*[[(t, c) for c in t.children] for t in sim.get_types])))
-
-    pos = {}
+    G.add_nodes_from([t for t in sim.get_types()])
+    G.add_edges_from(list(itertools.chain(*[[(t, c) for c in t.children] for t in sim.get_types()])))
 
     # Find largest type at end of simulation
-    t = max(sim.get_types, key=lambda t: t.history[-1][0])
+    # t = sim.get_max_type()
+    t = max(sim.get_types(), key=lambda t: t.size)
+    print('largest type at end: {}'.format(t))
     ts = t.parents
-    # Trace back to wildtype through largest parent Types
-    while t != sim.wildtype:
+    # Trace back to wildtype (type with no parents) through largest parent Types
+    # while t != sim.wildtype and len(t.parents) != 0:
+    for i in range(5):
+        if not (t != sim.wildtype and len(t.parents) != 0):
+            break
         prev_t = t
         t = max(ts, key=lambda x: x.max_size)
-
+        print('parents: {}'.format([t.name for t in ts]))
+        print('largest parent: {}'.format(t))
         if prev_t and t:
             G[prev_t][t]['color'] = 'r'
 
         ts = t.parents
 
-    for t in sim.get_types:
+    pos = {}
+    for t in sim.get_types():
         pos[t] = t.pos
 
     nx.draw(G, pos=pos, edge_color=[G[u][v].get('color', 'b') for u, v in G.edges()], with_labels=True)
@@ -84,18 +89,47 @@ def network_with_dominant(sim: Simulation, nx=nx) -> None:
 
 def network(sim: Simulation, nx=nx):
     G = nx.Graph()
-
-    G.add_nodes_from(sim.get_types)
-
-    G.add_edges_from(list(itertools.chain(*[[(t, c) for c in t.children] for t in sim.get_types])))
+    G.add_nodes_from(sim.get_types())
+    G.add_edges_from(list(itertools.chain(*[[(t, c) for c in t.children] for t in sim.get_types()])))
 
     pos = {}
-
-    for t in sim.get_types:
+    for t in sim.get_types():
         pos[t] = t.pos
 
     nx.draw(G, pos=pos, with_labels=True)
 
 
-def network_with_percentages(sims: List[Simulation], nx=nx):
-    pass
+def network_with_percentages(sim: Simulation, paths: List[List[Type]], nx=nx):
+    # TODO check simulations are all on the same types, just look at wildtype
+    # TODO check simulations all ran for the same time?
+
+    G = nx.Graph()
+    G.add_nodes_from([t for t in sim.get_types()])
+    G.add_edges_from(list(itertools.chain(*[[(t, c) for c in t.children] for t in sim.get_types()])))
+
+    num_paths = len(paths)
+
+    for p in paths:
+        for i, t in enumerate(p[:-1]):
+            t_next = p[i + 1]
+            if 'weight' in G[t][t_next]:
+                G[t][t_next]['weight'] += 1
+            else:
+                G[t][t_next]['weight'] = 1
+            if 'colour' not in G[t][t_next]:
+                G[t][t_next]['colour'] = 'r'
+
+    pos = {}
+    for t in sim.get_types():
+        pos[t] = t.pos
+
+    colours = [G[u][v].get('colour', 'b') for u, v in G.edges()]
+    weights = [G[u][v].get('weight', 1) for u, v in G.edges()]
+    edge_labels = {}
+    for u, v in G.edges():
+        weight = G[u][v].get('weight', 0)
+        if weight != 0:
+            edge_labels[(u, v)] = '{}%'.format((weight * 100) / num_paths)
+
+    nx.draw(G, pos=pos, edge_color=colours, width=weights, with_labels=True)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
