d4d1ac61476bc13e58978ac064b48b8689fe28a5
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-04-26 20:39:36 +0100

    [main] Some changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pickle
 from time import time, strftime
+from typing import List
 
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -13,7 +14,7 @@ from type import Type
 def mutations_simulation() -> Simulation:
     rates = {tuple('abc'): (10.0, 9.), tuple('Abc'): (10.0, 7.), tuple('ABc'): (10.0, 6.), tuple('ABC'): (10.0, 5.)}
 
-    sim = Generator.parameters(tuple('abc'), tuple('ABC'), tuple('ABD'), rates=rates, size=10000, wildtype_size=10000,
+    sim = Generator.parameters(tuple('abc'), [tuple('ABC'), tuple('ABD')], rates=rates, size=10000, wildtype_size=10000,
                                default_rate=(10.0, 9.))
 
     # for t in sim.get_types:
@@ -24,28 +25,31 @@ def mutations_simulation() -> Simulation:
     return sim
 
 
-def save_simulation(sim: Simulation, name: str = None):
+def save_path(sim: Simulation, name: str = None):
     if not name:
         name = strftime("%Y%m%d-%H%M%S")
+    else:
+        name.replace('[TIME]', strftime("%Y%m%d-%H%M%S"))
 
     with open('{}.sim'.format(name), 'wb') as f:
-        pickle.dump(sim, f, -1)
+        pickle.dump(sim.get_dominant_path(), f, -1)
 
 
-def load_simulation(name: str) -> Simulation:
+def load_path(name: str) -> List[Type]:
     t0 = time()
-    with open(name, 'rb') as f:
+    with open('{}.sim'.format(name), 'rb') as f:
         p = pickle.load(f)
         print("Loading simulation complete in {:f}s".format(time() - t0))
         return p
 
 
-def display_simulation(sim):
+def display_simulation(sim: Simulation):
+    # data_plot.stacked_plot(sim.get_types(), sim.get_tmax(), sim.get_pop_max(), plt)
+    # plt.figure()
+    # data_plot.line_plot(sim.get_types(), sim.get_tmax(), sim.get_pop_max(), plt)
+    # plt.figure()
     data_plot.network_with_dominant(sim, nx)
-    plt.figure()
-    data_plot.line_plot(sim.get_types, sim.get_tmax, sim.get_pop_max, plt)
-    plt.figure()
-    data_plot.stacked_plot(sim.get_types, sim.get_tmax, sim.get_pop_max, plt)
+    # data_plot.network(sim, nx)
     plt.show()
 
 
@@ -61,15 +65,31 @@ def one_type_exp():
 
     sim.run(.3)
 
-    data_plot.stacked_plot(sim.get_types, sim.get_tmax, sim.get_pop_max, plt)
+    data_plot.stacked_plot(sim.get_types(), sim.get_tmax(), sim.get_pop_max(), plt)
+    plt.show()
+
+
+def graph_multiple_sims():
+    paths = [load_path('10.0_ABC_D'), load_path('10.0_ABC_D_2'), load_path('10.0_ABC_D_3'),
+             load_path('3.0_ABC_D')]
+
+    rates = {tuple('abc'): (10.0, 9.), tuple('Abc'): (10.0, 7.), tuple('ABc'): (10.0, 6.), tuple('ABC'): (10.0, 5.)}
+
+    sim = Generator.parameters(tuple('abc'), [tuple('ABC'), tuple('ABD')], rates=rates, size=10000, wildtype_size=10000,
+                               default_rate=(10.0, 9.))
+
+    data_plot.network_with_percentages(sim, paths, nx)
     plt.show()
 
 
 if __name__ == '__main__':
-    sim = mutations_simulation()
-    save_simulation(sim, 'full_10.0_ABC_D')
-    # sim = load_simulation('20180425-194817.sim')
-    # display_simulation(sim)
+    graph_multiple_sims()
+
+# sim = mutations_simulation()
+# save_simulation(sim, '10.0_ABC_D_3')
+
+# sim = load_simulation('10.0_ABC_D_3')
+# display_simulation(sim)
 
 # mutation = Generator.Mutation
 # mutation_rates = {
