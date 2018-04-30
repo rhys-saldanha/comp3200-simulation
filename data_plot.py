4cd905a052e76794b598eea44011d27b63ee42a3
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-04-30 16:18:59 +0100

    [PLT] Use one function to draw networks

--- data_plot.py
+++ data_plot.py
@@ -8,6 +8,8 @@ from simulation import Simulation
 from type import Type
 
 resolution = 1000
+default_colour = 'gray'
+dominant_colour = 'r'
 
 
 def line_plot(sim: Simulation, plt=plt, do_reduce=True):
@@ -49,50 +51,15 @@ def stacked_plot(types: List[Type], tmax: float, size_max: int, plt=plt):
     plt.xlim((0, tmax))
 
 
-def network_with_dominant(sim: Simulation, nx=nx) -> None:
-    G = nx.Graph()
-    G.add_nodes_from([t for t in sim.get_types()])
-    G.add_edges_from(list(itertools.chain(*[[(t, c) for c in t.children] for t in sim.get_types()])))
-
-    # Find largest type at end of simulation
-    # t = sim.get_max_type()
-    t = max(sim.get_types(), key=lambda t: t.size)
-    # print('largest type at end: {}'.format(t))
-    ts = t.parents
-    # Trace back to wildtype (type with no parents) through largest parent Types
-    # while t != sim.wildtype and len(t.parents) != 0:
-    for i in range(5):
-        if not (t != sim.wildtype and len(t.parents) != 0):
-            break
-        prev_t = t
-        t = max(ts, key=lambda x: x.max_size)
-        # print('parents: {}'.format([t.name for t in ts]))
-        # print('largest parent: {}'.format(t))
-        if prev_t and t:
-            G[prev_t][t]['color'] = 'r'
-
-        ts = t.parents
-
-    pos = {}
-    for t in sim.get_types():
-        pos[t] = t.pos
-
-    nx.draw(G, pos=pos, edge_color=[G[u][v].get('color', 'b') for u, v in G.edges()], with_labels=True)
+def network(sim: Simulation, nx=nx) -> nx.Graph:
+    return network_with_percentages(sim, [], percentages=False, nx=nx)
 
 
-def network(sim: Simulation, nx=nx):
-    G = nx.Graph()
-    G.add_nodes_from(sim.get_types())
-    G.add_edges_from(list(itertools.chain(*[[(t, c) for c in t.children] for t in sim.get_types()])))
-
-    pos = {}
-    for t in sim.get_types():
-        pos[t] = t.pos
-
-    nx.draw(G, pos=pos, with_labels=True)
+def network_with_dominant(sim: Simulation, nx=nx) -> nx.Graph:
+    return network_with_percentages(sim, [sim.get_dominant_path()], percentages=False, nx=nx)
 
 
-def network_with_percentages(sim: Simulation, paths: List[List[Type]], nx=nx) -> nx.Graph:
+def network_with_percentages(sim: Simulation, paths: List[List[Type]], percentages=True, nx=nx) -> nx.Graph:
     G = nx.Graph()
     G.add_nodes_from([t for t in sim.get_types()])
     G.add_edges_from(list(itertools.chain(*[[(t, c) for c in t.children] for t in sim.get_types()])))
@@ -108,24 +75,25 @@ def network_with_percentages(sim: Simulation, paths: List[List[Type]], nx=nx) ->
             else:
                 G[t][t_next]['weight'] = 1
             if 'colour' not in G[t][t_next]:
-                G[t][t_next]['colour'] = 'r'
+                G[t][t_next]['colour'] = dominant_colour
         for t in p:
             if t not in node_colour:
-                node_colour[t] = 'r'
+                node_colour[t] = dominant_colour
 
     pos = {}
     for t in sim.get_types():
         pos[t] = t.pos
 
-    edge_colour = [G[u][v].get('colour', 'g') for u, v in G.edges()]
+    edge_colour = [G[u][v].get('colour', default_colour) for u, v in G.edges()]
     weights = [G[u][v].get('weight', 1) for u, v in G.edges()]
-    node_colour = [node_colour.get(u, 'g') for u in G]
+    node_colour = [node_colour.get(u, default_colour) for u in G]
 
     edge_labels = {}
-    for u, v in G.edges():
-        weight = G[u][v].get('weight', 0)
-        if weight != 0:
-            edge_labels[(u, v)] = '{}%'.format((weight * 100) / num_paths)
+    if percentages:
+        for u, v in G.edges():
+            weight = G[u][v].get('weight', 0)
+            if weight != 0:
+                edge_labels[(u, v)] = '{}%'.format((weight * 100) / num_paths)
 
     nx.draw(G, pos=pos, node_color=node_colour, edge_color=edge_colour, width=weights, with_labels=True, node_size=2500)
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
