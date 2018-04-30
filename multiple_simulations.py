15de964d357065f9d2c9b793743ba7225732b135
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-04-30 02:27:45 +0100

    [MUL] Fix multiple simulations
    * check for data folder first
    * name files better in line with useful.load_paths function
    * use one less CPU than reported so that OS etc doesn't freeze

--- multiple_simulations.py
+++ multiple_simulations.py
@@ -1,30 +1,41 @@
 import functools
+import os
 import pickle
 from multiprocessing import Pool, cpu_count
 
 from simulation import Simulation
 from simulation_generator import Generator
 
-NAME = 'data/10.0_abc_ABC_D_{}'
-PARAMETERS = 'parameters/abc_ABC_D.json'
-SIM_NUM = 16
+PARAMETERS = 'two_paths'
+SIM_NUM = 30
 TIME = 10.0
+PRINT = False
 
 
 def dominant_paths(sim: Simulation, name: str) -> int:
     sim = sim.clone()
-    # print(sim.wildtype, name)
+    print('Running {}'.format(name))
     sim.run(TIME)
-    with open('{}.sim'.format(name), 'wb') as f:
-        pickle.dump(sim.get_dominant_path(), f, -1)
+    with open('{}.sim'.format(name), 'wb') as f_sim:
+        pickle.dump(sim.get_dominant_path(), f_sim, -1)
     return 0
 
 
 if __name__ == '__main__':
-    sim = Generator.config_file(PARAMETERS)
-    with Pool(processes=cpu_count()) as pool:
-        simulations = [NAME.format(i) for i in range(SIM_NUM)]
+    print('Simulating {} {} times for {} time'.format(PARAMETERS, SIM_NUM, TIME))
+    print('Found {} CPUs'.format(cpu_count()))
 
-        f = functools.partial(dominant_paths, sim)
+    # Make folder for data if it doesn't exist
+    os.makedirs('data/', exist_ok=True)
+
+    g = Generator(print=PRINT)
+
+    s = g.config_file('parameters/' + PARAMETERS)
+    with Pool(processes=cpu_count() - 1) as pool:
+        simulations = ['data/' + PARAMETERS + '_{}'.format(i) for i in range(SIM_NUM)]
+
+        f = functools.partial(dominant_paths, s)
 
         pool.map(f, simulations)
+
+    print('Finished!')
