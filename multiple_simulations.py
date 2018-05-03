2f73022a3a9c39d0e6e6884ab0cfba3eee1f93cb
RHYS SALDANHA <rhys.saldanha@gmail.com>
2018-05-03 18:06:32 +0100

    [MUL] Use commandline arguments

--- multiple_simulations.py
+++ multiple_simulations.py
@@ -1,35 +1,40 @@
+import argparse
 import functools
 import os
 import pickle
 import time
 from multiprocessing import Pool, cpu_count
-from typing import List, Union
+from typing import List
 
-import matplotlib.pyplot as plt
-import networkx as nx
-
-import data_plot
 from simulation import Simulation
 from simulation_generator import Generator
 from type import Type
 
-PARAMETERS: List[str] = ['two_paths_ABC_D_more_separate_skewed_1_5']
-SIM_NUM = 100
-TIME = 10.0
-PRINT = False
+parser = argparse.ArgumentParser('multiple_simulations')
+parser.add_argument('-c', nargs='+', help='config file(s) for simulation in JSON format', required=True)
+parser.add_argument('-n', help='number of simulations to run', type=int, default=100)
+parser.add_argument('-t', help='time input for Simulation.run()', type=float, required=True)
+parser.add_argument('-v', help='verbose mode, prints a lot more but will get messy', type=bool, default=False)
+args = parser.parse_args()
+
+PARAMETERS: List[str] = args.c
+SIM_NUM: int = args.n
+TIME: float = args.t
+PRINT: bool = args.v
 
 
 def dominant_path(sim: Simulation, name: str) -> List[Type]:
     sim = sim.clone()
     # print('Running {}'.format(name))
     sim.run(TIME)
-    print('Finished {}'.format(name))
+    if PRINT:
+        print('Finished {}'.format(name))
     return sim.get_dominant_path()
 
 
-def run_multiple_simulations(parameters: str):
+def run_multiple_simulations(config: str):
     g = Generator(prints=PRINT)
-    s = g.config_file('parameters/' + parameters)
+    s = g.config_file(config)
 
     # Showing graphs messes with threading, only use to test simulation parameters
     # data_plot.network(s, nx)
@@ -44,11 +49,12 @@ def run_multiple_simulations(parameters: str):
     # data_plot.line_plot(s_dup, plt)
     # plt.show()
 
+    print('Simulating {} {} times for {} time'.format(config, SIM_NUM, TIME))
     print('Starting at {}'.format(time.strftime("%Y-%m-%d %H:%M:%S")))
     t0 = time.time()
 
     with Pool(maxtasksperchild=2) as pool:
-        simulations = [parameters + '_{}'.format(i) for i in range(SIM_NUM)]
+        simulations = [config + '_{}'.format(i) for i in range(SIM_NUM)]
 
         f = functools.partial(dominant_path, s)
 
@@ -57,16 +63,16 @@ def run_multiple_simulations(parameters: str):
     print('Finished simulations at {}'.format(time.strftime("%Y-%m-%d %H:%M:%S")))
     print('Took {}s'.format(time.time() - t0))
 
-    with open('data/{}.sim'.format(parameters), 'wb') as f_sim:
+    with open('data/{}.sim'.format(config), 'wb') as f_sim:
         pickle.dump(result, f_sim, -1)
-    print('Finished!')
 
 
 if __name__ == '__main__':
-    print('Simulating {} {} times for {} time'.format(PARAMETERS, SIM_NUM, TIME))
     print('Found {} CPUs'.format(cpu_count()))
 
     # Make folder for data if it doesn't exist
     os.makedirs('data/', exist_ok=True)
 
     [run_multiple_simulations(p) for p in PARAMETERS]
+
+# python multiple_simulations.py -c parameters/abc_ABC_D.json -t 1.0 -v true
